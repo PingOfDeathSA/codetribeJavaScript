807cf52cde1c81bd6f691958cdf4377d12234173
RonaldSiyangoma <ronaldnt8@gmail.com>
2025-04-10 18:31:06 +0200

    bill change
    bill change

--- Bill_Division.js
+++ Bill_Division.js
@@ -1,16 +1,18 @@
+let bill = [3, 10, 2, 9];
+let k = 1;
+let b = 12;
+
 function bonAppetit(bill, k, b) {
     let totalBill = bill.reduce((acc, curr) => acc + curr, 0);
     let annaShare = (totalBill - bill[k]) / 2;
 
     if (b === annaShare) {
-        console.log("Bon Appetit");
+       // console.log("Bon Appetit");
     } else {
         console.log(b - annaShare);
     }
 }
 
-let bill = [3, 10, 2, 9];
-let k = 1;
-let b = 12;
+
 
 bonAppetit(bill, k, b);
